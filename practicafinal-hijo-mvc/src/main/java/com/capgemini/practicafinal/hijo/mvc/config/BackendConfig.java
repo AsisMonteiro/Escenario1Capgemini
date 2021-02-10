0b63ea24b28fa228035a3d9e8f30d0e72b357add
Asis Monteiro <asis_torre@hotmail.com>
2021-02-10 14:08:07 +0100

    unidad de persistencia configurada en rootconfig

--- practicafinal-hijo-mvc/src/main/java/com/capgemini/practicafinal/hijo/mvc/config/BackendConfig.java
+++ practicafinal-hijo-mvc/src/main/java/com/capgemini/practicafinal/hijo/mvc/config/BackendConfig.java
@@ -1,7 +1,12 @@
 package com.capgemini.practicafinal.hijo.mvc.config;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 /*
  * Clase de configuracion de Spring
@@ -23,5 +28,30 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = { "com.capgemini.practicafinal.hijo.mvc", "com.capgemini.practicafinal.hijo.rest",
 		"com.capgemini.practicafinal.hijo.data" })
 public class BackendConfig {
+
+	@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory() {
+		LocalEntityManagerFactoryBean factory = new LocalEntityManagerFactoryBean();
+
+		/*
+		 * Cargar la unidad de persistencia del fichero persistence.xml
+		 * 
+		 * jpa-mysql => MySQL
+		 * 
+		 * jpa-h2 => H2
+		 */
+		factory.setPersistenceUnitName("jpa-mysql" /* "jpa-h2" */);
+
+		return factory;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+
+		txManager.setEntityManagerFactory(entityManagerFactory);
+
+		return txManager;
+	}
 
 }
